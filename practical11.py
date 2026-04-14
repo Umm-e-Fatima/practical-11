d13f139a32cae86c0d2d7cc48c8cf818f8f21aa4
Umm E Fatima <fatimaanis809@gmail.com>
2026-04-14 08:51:55 +0500

    file added practical11

---
+++ practical11.py
@@ -0,0 +1,11 @@
+# def greet(name):
+#     print("Hello " + name)
+#
+# greet("fatima")
+
+def my_function():
+    x = 10 # Local variable
+    print(x)
+
+my_function() # Output: 10
+print(x) # Error: x is not defined outside the function
